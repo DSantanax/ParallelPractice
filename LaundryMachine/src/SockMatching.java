42a62b43c9bd3f27311595ea31d08f7c50983e73
Daniel Santana <santana.danny4g@gmail.com>
2020-02-15 22:08:35 -0800

    Test 500

--- LaundryMachine/src/SockMatching.java
+++ LaundryMachine/src/SockMatching.java
@@ -7,16 +7,19 @@ public class SockMatching {
     private static String[] colors = { "Blue", "Red", "Orange", "Green" };
 
     public static void main(String[] args) {
-        BlockingQueue<String> bq = new ArrayBlockingQueue<>(500);
-        BlockingQueue<String> destroyer = new ArrayBlockingQueue<>(500);
-
-        int rand = 10;
         Random rn = new Random();
+        int frstRand = rn.nextInt(100);
+        int scdRand = rn.nextInt(100);
+        int thrdRand = rn.nextInt(100);
+        int fthRand = rn.nextInt(100);
+        int sum = frstRand + scdRand + thrdRand + fthRand;
+        BlockingQueue<String> bq = new ArrayBlockingQueue<>(sum);
+        BlockingQueue<String> destroyer = new ArrayBlockingQueue<>(sum);
 
-        Sock bl = new Sock(rn.nextInt(rand) + 1, colors[0], bq, destroyer);
-        Sock rd = new Sock(rn.nextInt(rand) + 1, colors[1], bq, destroyer);
-        Sock org = new Sock(rn.nextInt(rand) + 1, colors[2], bq, destroyer);
-        Sock grn = new Sock(rn.nextInt(rand) + 1, colors[3], bq, destroyer);
+        Sock bl = new Sock(rn.nextInt(frstRand) + 1, colors[0], bq, destroyer);
+        Sock rd = new Sock(rn.nextInt(scdRand) + 1, colors[1], bq, destroyer);
+        Sock org = new Sock(rn.nextInt(thrdRand)+ 1, colors[2], bq, destroyer);
+        Sock grn = new Sock(rn.nextInt(fthRand) + 1, colors[3], bq, destroyer);
 
         Thread blue = new Thread(new SockProducer(bl));
         blue.start();
